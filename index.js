35f517b3c32ab5ad933093c0f1e7348eb6ed0829
Jagad R R <jagadraya388@gmail.com>
2022-10-22 19:31:34 +0700

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static('assets'))
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
