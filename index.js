6ee9ebae223f67c4bbf4f03004ca4a0ad7140e13
Jagad R R <jagadraya388@gmail.com>
2022-10-22 19:40:19 +0700

    Update

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('assets'))
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html')
+	res.render('index')
 })
 
 app.get('/camera', (req, res) => {
-	res.sendFile(__dirname + '/camera.html')
+	res.render('camera')
+})
+
+app.use((req, res, next) => {
+   res.status(404).send('<h1>404 Not Found</h1>');
 })
 
 app.listen(port, () => {
