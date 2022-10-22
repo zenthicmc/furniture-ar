b3b2c73cbd48bd5c43afb59c88f6c37bfb4b8609
Jagad R R <jagadraya388@gmail.com>
2022-10-22 19:56:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 3000
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
+app.set('views', __dirname + '/../views');
+
 app.get('/', (req, res) => {
 	res.render('index')
 })
